3b897c18ce48475bd7be2db90a06668d6f4b8569
Trong Truong <tntrong@tma.com.vn>
2024-06-12 15:09:32 +0700

    udpate code

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,36 +1,11 @@
-// pages/blog/[slug].tsx
-
-import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
-
 type Params = {
   params: {
     slug: string;
   };
 };
 
-export async function generateStaticParams() {
-  const slugs = await fetchBlogSlugs();
-  return slugs.map(slug => ({
-    params: { slug }
-  }));
-}
-
-async function fetchBlogSlugs() {
-  // Replace this with your actual data fetching logic
-  return ['slug1', 'slug2', 'slug3'];
-}
-
 export async function generateMetadata({ params }: Params) {
   return { title: `Post: ${params.slug}` };
-}
-
-export async function getStaticProps(context: GetStaticPropsContext) {
-  const { params } = context;
-  return {
-    props: {
-      params
-    }
-  };
 }
 
 export default function Page({ params }: Params) {
